e29f076eb8231d15b2b0f3d089fb7cd64be3a314
Ejeh <ejehsax@gmail.com>
2025-03-29 12:53:58 -0400

    testing

--- HotelManagementMVC/src/main/java/org/ejeh/hotelmanagementmvc/HotelManagementMvcApplication.java
+++ HotelManagementMVC/src/main/java/org/ejeh/hotelmanagementmvc/HotelManagementMvcApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelManagementMvcApplication {
-
+        //Testing
     public static void main(String[] args) {
         SpringApplication.run(HotelManagementMvcApplication.class, args);
     }
+
 
 }
