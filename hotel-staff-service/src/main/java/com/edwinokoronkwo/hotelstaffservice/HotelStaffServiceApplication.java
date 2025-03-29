c53b545e0271aaf2cbd3f3b735064de2debb622c
Ejeh <ejehsax@gmail.com>
2025-03-29 12:59:55 -0400

    testing testing

--- hotel-staff-service/src/main/java/com/edwinokoronkwo/hotelstaffservice/HotelStaffServiceApplication.java
+++ hotel-staff-service/src/main/java/com/edwinokoronkwo/hotelstaffservice/HotelStaffServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HotelStaffServiceApplication {
-
+    //testing
     public static void main(String[] args) {
         SpringApplication.run(HotelStaffServiceApplication.class, args);
     }
