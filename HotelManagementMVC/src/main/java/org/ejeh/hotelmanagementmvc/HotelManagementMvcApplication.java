b5abcda87bad10f889235015b31b1940d87d09b2
Ejeh <ejehsax@gmail.com>
2025-03-29 12:05:11 -0400

    init commit correction

---
+++ HotelManagementMVC/src/main/java/org/ejeh/hotelmanagementmvc/HotelManagementMvcApplication.java
@@ -0,0 +1,13 @@
+package org.ejeh.hotelmanagementmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementMvcApplication.class, args);
+    }
+
+}
---
+++ HotelManagementMVC/src/test/java/org/ejeh/hotelmanagementmvc/HotelManagementMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ejeh.hotelmanagementmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
